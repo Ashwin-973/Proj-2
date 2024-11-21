004c5183474ee977b8edb8d50e045da2ec3577ce
Tony Montana <ashwin.s1336@gmail.com>
2024-11-21 10:53:35 +0530

    Ws upgrade successful from the FE

---
+++ Day 4/HacksawRidge.js
@@ -0,0 +1,39 @@
+
+//opening chatbox on the click of every username
+let usersNodeList = document.querySelectorAll(".chat-list")
+let users = Array.from(usersNodeList)
+console.log(users)
+let popUp=document.getElementById('right-nav')
+const exeOnClick= (u) => {
+  u.addEventListener("click", () =>
+  {
+    console.log("Clicked")
+    if(popUp.style.display==='none')
+    {
+      popUp.style.display='flex'
+    }
+    else
+    { 
+      popUp.style.display='none'
+    }
+  })
+}
+users.forEach(exeOnClick)
+//instatiating a websocket connection between browser API and server side web socket
+const websocket=new WebSocket('ws://localhost:8080')
+websocket.addEventListener('error',(e)=>
+{
+  console.log('there was an error from the client-side: ',e)
+})
+
+fetch('http:/localhost:8080',{method:'GET',mode:'cors'})
+.then((msg)=>
+{
+  console.log('GET request got a response')
+  console.log('it was: ',msg)
+  
+})
+.catch(()=>
+{
+  console.log('there was an error durin res')
+})
