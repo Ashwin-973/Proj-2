5d3a020a957bc5c68275637c7b15f75c2fddffd1
Tony Montana <ashwin.s1336@gmail.com>
2024-11-22 07:34:55 +0530

    Message routed to server

---
+++ Day 5/HacksawRidge.js
@@ -0,0 +1,40 @@
+
+//opening chatbox on the click of every username
+let usersNodeList = document.querySelectorAll(".chat-list")
+let users = Array.from(usersNodeList)
+console.log(users)
+let popUp=document.getElementById('right-nav')
+const exeOnClick= (u) => {
+  u.addEventListener("click", () =>
+  {
+    console.log("Clicked")
+    if(popUp.style.display==='none')
+    {
+      popUp.style.display='flex'
+    }
+    else
+    { 
+      popUp.style.display='none'
+    }
+  })
+}
+users.forEach(exeOnClick)
+//instatiating a websocket connection between browser API and server side web socket
+const websocket=new WebSocket('ws://localhost:8080')
+
+
+websocket.addEventListener('open',()=>
+  {
+    console.log('connection open')
+    document.getElementById('send').addEventListener('click',()=>
+    {
+      websocket.send(document.getElementById('messageBox').value)
+      document.getElementById('messageBox').value=''
+      // document.querySelector('#messageBox').setAttribute('value','none')
+    })
+  })
+websocket.addEventListener('error',(e)=>
+ {
+    console.log('there was an error from the client-side: ',e)
+ })
+
