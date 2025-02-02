e18b955099b2d37c8c64c56f992f8b17ee9414d7
Tony Montana <ashwin.s1336@gmail.com>
2025-02-02 12:18:31 +0530

    Untracked Day 1

--- Day 1/HacksawRidge.js
+++
@@ -1,9 +0,0 @@
-let usersNodeList = document.querySelectorAll(".chat-list")
-let users = Array.from(usersNodeList)
-console.log(users)
-const exeOnClick= (u) => {
-  u.addEventListener("click", () => {
-    console.log("Clicked")
-  })
-}
-users.forEach(exeOnClick)
