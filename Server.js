bf02501e5d328209c7b8bf47eb3b7a38e71cc7da
Tony Montana <ashwin.s1336@gmail.com>
2025-02-18 09:40:11 +0530

    refactor: separate socket server and app logic, added cookie parser and auth routes

--- Server.js
+++ Server.js
@@ -1,13 +1,18 @@
-const SocketIO=require('socket.io')
+//!separate this into server.js and app.js
 const http=require('http')
+const cookieParser=require('cookie-parser')
 const express=require('express')
 const app=express()
 const userRoute=require("./routes/user")
+const authRoute=require('./routes/authRoutes')
+const {initializeServerSocket}=require('./sockets/socketServer')
 const server=http.createServer(app)
 server.listen(8080,()=>
 {
     console.log(server.address())
 })
+app.use(cookieParser())
+initializeServerSocket(server)
 app.get('/',(req,res)=>
 {
     res.send("welcome");
@@ -15,34 +20,14 @@ app.get('/',(req,res)=>
 app.set('views',"C:\\Users\\Ashwi\\Documents\\Web Development\\WebDEV\\Proj2\\views")
 app.set('view engine','ejs')
 app.use(express.static("C:\\Users\\Ashwi\\Documents\\Web Development\\WebDEV\\Proj2\\public"))
-// why won't this work
 app.use((req,res,next)=>
 {
     res.setHeader('Cache-Control','no-cache')
     res.setHeader('Expires','0')
     next()
 })
-//Route navigator: control directed to user.js under Router whenever the endpoint of the base url is /users
+//**Route navigator: control directed to user.js under Router whenever the endpoint of the base url is /users
 app.use('/users',userRoute)
-// app.use(express.urlencoded({extended:true}))
-
-
-//web socket server with a ws server  instance for a particular client 
-const SocketServer=new SocketIO.Server(server,
-    {
-        cors:{origin:'*'}
-    })
-SocketServer.on('connection',(socket)=>
-{
-    console.log(`A client connected to the web socket server with the id:${socket.id}`)
-    SocketServer.emit('passingID',socket.id)
-    socket.on('message',(msg)=>{
-        console.log('message is:',msg)
-        //broadcasts to all clients
-        let messageObj={sessionID:socket.id,message:msg}
-        SocketServer.emit('message',messageObj)
-        messageObj={}
-    })
-    
-})
+app.use('/auth',authRoute)
+//app.use(express.urlencoded({extended:true}))
 
